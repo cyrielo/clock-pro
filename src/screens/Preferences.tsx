b5181f19adaccdda01635444b417a34736bf17e0
Cyril Ologho <cyrielo911@gmail.com>
2025-01-08 18:31:03 -0500

    update preferences screen

--- src/screens/Preferences.tsx
+++ src/screens/Preferences.tsx
@@ -1,94 +1,92 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { useState } from 'react';
-import { View, Text, Switch, ScrollView, Modal, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Switch, ScrollView, Modal, TouchableOpacity, StyleSheet, Pressable, Keyboard, Button } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import Section from '../components/Section';
 import Ionicon from '@react-native-vector-icons/ionicons';
 import { Dropdown } from 'react-native-element-dropdown';
+import { observer } from 'mobx-react-lite';
+import { NotificationSounds, THEMES, LANGUAGES } from '../constants';
+import { PreferencesStore } from '../store/';
 const IconSize = 24;
 const fontSize = 16;
 
-const themes = [{
-  label: 'System',
-  value: 'system'
-}, {
-  label :'Light',
-  value: 'light'
-},{
-  label: 'Dark',
-  value: 'Dark'
-}];
-
-const notificationSounds = [{
-  label: 'Default',
-  value: 'default'
-},{
-  label: 'Gentle stream',
-  value: 'gentle_stream'
-}, {
-  label: 'Telephonica',
-  value: 'telephonica'
-}, {
-  label: 'Morning Rooster',
-  value: 'morning_rooster'
-}];
-
-
-const languages = [{
-  label: 'English',
-  value: 'en'
-},{
-  label: 'French',
-  value: 'fr'
-},{
-  label: 'Chinese',
-  value: 'ch'
-}];
-
 const ModalStyle = StyleSheet.create({
   modal: {
-    marginHorizontal: 20,
+    marginHorizontal: 15,
     marginVertical: 'auto',
-    borderWidth: 2,
     backgroundColor: 'white',
     minHeight: 200,
-    padding: 15,
-    borderRadius: 10,
+    maxHeight: 600,
+    padding: 10,
+    borderRadius: 8,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
   }
 });
+const lorem = 
+`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae turpis at turpis egestas ultricies a luctus ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc feugiat ullamcorper velit, in feugiat dolor convallis in. Quisque porttitor metus non nibh sodales finibus. Vivamus ornare, nunc ac vulputate cursus, mauris sapien pellentesque lacus, et venenatis mi ante at nunc. Sed semper fermentum mollis. Nullam non ante malesuada, pretium orci in, dapibus ex. Curabitur ut justo odio. Donec pretium dictum sollicitudin. Fusce elementum euismod mauris, nec condimentum velit varius sit amet. Mauris fringilla purus at lacus consequat, vel dictum sapien scelerisque.
+auris fringilla purus at lacus consequat, vel dictum sapien scelerisque.
+`
 
-const d = () => { return (<Text>Hi</Text>) };
-
-const Preferences = (() => {
-  const [theme, setTheme] = useState('light');
+const Preferences = observer( () => {
   const [modalVisibility, setModalVisibility] = useState(false);
-  const [language, setLanguage] = useState('en');
-  const [notificationEnabled, setNotificationEnabled] = useState(false);
-  const [notificationSound, setNotificationSound] = useState('default');
+  const prefs = PreferencesStore.preferences;
+
+  const toggelModal = () => {
+    Keyboard.dismiss();
+    setModalVisibility(!modalVisibility);
+    console.log('modal set to ', modalVisibility);
+  }
+
   return (
+
     <SafeAreaProvider>
       <SafeAreaView>
-        <Modal
-          visible={modalVisibility}
-          transparent={true}
-          animationType='fade'
-          onRequestClose={() => {
-            setModalVisibility(false);
-          }}
+          <Modal
+            visible={modalVisibility}
+            transparent={true}
+            animationType='fade'
+          onRequestClose={toggelModal}
+            style={{ position: 'relative', zIndex:0, borderWidth: 10, borderColor: 'red', backgroundColor: 'red' }}
           >
-            <View style={ModalStyle.modal}>
-              <Text>Modal content</Text>
-            </View>
-        </Modal>
-        <ScrollView showsVerticalScrollIndicator={false}>
+            <Pressable
+              style={{
+              flex: 1,
+              backgroundColor: "rgba(0,0,0,.1)", // Dimmed background
+              justifyContent: "center",
+              alignItems: "center",
+              }}
+            onPress={toggelModal}
+            >
+            <Pressable onPress={() => {}}>
+              <ScrollView style={{
+                ...ModalStyle.modal,
+                flexGrow: 0,
+                }}>
+                <Text style={{
+                  marginBottom: 25,
+                }}>
+                  {lorem}
+                </Text>
+              <Button
+                title="Close"
+                  onPress={toggelModal}
+              />
+              </ScrollView>
+            </Pressable>
+          </Pressable>
+
+          </Modal>
+        <ScrollView
+          showsVerticalScrollIndicator={false}>
           <View style={{
             margin: 20,
             marginBottom: 0,
@@ -103,19 +101,19 @@ const Preferences = (() => {
               paddingBottom: 15,
               marginBottom: 20,
             }}>
-            <View style={{
-              flexDirection: 'row',
-              flex: 1,
-              alignItems: 'center'
+              <View style={{
+                flexDirection: 'row',
+                flex: 1,
+                alignItems: 'center'
               }}>
-              <Ionicon style={{ marginRight: 10 }} name='contrast-outline' size={IconSize} />
-              <Text style={{ fontSize }}>Theme</Text>
-            </View>
-            <View style={{
-              flexDirection: 'row',
-              flex: 1,
-              justifyContent: 'flex-end',
-              alignItems: 'center'
+                <Ionicon style={{ marginRight: 10 }} name='contrast-outline' size={IconSize} />
+                <Text style={{ fontSize }}>Theme</Text>
+              </View>
+              <View style={{
+                flexDirection: 'row',
+                flex: 1,
+                justifyContent: 'flex-end',
+                alignItems: 'center'
               }}>
                 <Dropdown
                   style={{
@@ -123,18 +121,18 @@ const Preferences = (() => {
                     width: '100%',
                     justifyContent: 'flex-end',
                   }}
-                  selectedTextStyle={{textAlign: 'right'}}
-                  data={themes}
+                  selectedTextStyle={{ textAlign: 'right' }}
+                  data={THEMES}
                   search={false}
-                  value={theme}
+                  value={prefs.theme}
                   onChange={(item) => {
-                    setTheme(item.value);
+                    PreferencesStore.setPreferences({ ...prefs, theme: item.value });
                   }}
                   renderRightIcon={() => (<Ionicon style={{}} name='chevron-forward-outline' size={IconSize} />)}
                   labelField={'label'}
                   valueField={'value'}
                 />
-            </View>
+              </View>
             </View>
             <View style={{
               flexDirection: 'row',
@@ -147,7 +145,7 @@ const Preferences = (() => {
                 flexDirection: 'row',
                 flex: 1,
                 alignItems: 'center'
-                }}>
+              }}>
                 <Ionicon style={{ marginRight: 10 }} name='language' size={IconSize} />
                 <Text style={{ fontSize: 16 }}>Language</Text>
               </View>
@@ -164,11 +162,11 @@ const Preferences = (() => {
                     justifyContent: 'flex-end',
                   }}
                   selectedTextStyle={{ textAlign: 'right' }}
-                  data={languages}
+                  data={LANGUAGES}
                   search={false}
-                  value={language}
-                  onChange={(item) => {
-                    setLanguage(item.value);
+                  value={prefs.language}
+                  onChange={({value}) => {
+                    PreferencesStore.setPreferences({ ...prefs, language: value });
                   }}
                   renderRightIcon={() => (<Ionicon style={{}} name='chevron-forward-outline' size={IconSize} />)}
                   labelField={'label'}
@@ -200,8 +198,10 @@ const Preferences = (() => {
                 alignItems: 'center'
               }}>
                 <Switch
-                  value={notificationEnabled}
-                  onValueChange={(val:boolean) => { setNotificationEnabled(val) }} />
+                  value={prefs.notificationEnabled}
+                  onValueChange={(val: boolean) => {
+                    PreferencesStore.setPreferences({ ...prefs, notificationEnabled: !!val });
+                  }} />
               </View>
             </View>
             <View style={{
@@ -233,11 +233,11 @@ const Preferences = (() => {
                     justifyContent: 'flex-end',
                   }}
                   selectedTextStyle={{ textAlign: 'right' }}
-                  data={notificationSounds}
+                  data={NotificationSounds}
                   search={false}
-                  value={notificationSound}
+                  value={prefs.notificationSound}
                   onChange={(item) => {
-                    setNotificationSound(item.value);
+                    PreferencesStore.setPreferences({ ...prefs, notificationSound: item.value });
                   }}
                   renderRightIcon={() => (<Ionicon style={{}} name='chevron-forward-outline' size={IconSize} />)}
                   labelField={'label'}
@@ -280,17 +280,17 @@ const Preferences = (() => {
                 alignItems: 'center',
                 marginTop: 20
               }}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={toggelModal}>
                   <Text style={{ fontSize, marginVertical: 5, }}>
                     Show Terms &amp; Condition
                   </Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={toggelModal}>
                   <Text style={{ fontSize, marginVertical: 5, }}>
                     Show Privacy Policy
                   </Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={toggelModal}>
                   <Text style={{ fontSize, marginVertical: 5, }}>
                     Show Third-Party Software
                   </Text>
@@ -301,7 +301,7 @@ const Preferences = (() => {
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
-)
+  )
 })
 
 const PreferenceStackNavigator = createNativeStackNavigator();
@@ -320,4 +320,3 @@ export const PreferenceStackScreen = () => {
 }
 
 export default Preferences;
-

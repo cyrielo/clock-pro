bdd4d7f2d91fe59323ff3c12cf20b7f0e46b595f
Cyril Ologho <cyrielo911@gmail.com>
2025-01-15 16:29:52 -0500

    update clock main screen

--- src/screens/Clock.tsx
+++ src/screens/Clock.tsx
@@ -1,15 +1,19 @@
 import React, { PropsWithChildren, useState } from 'react';
 import { format,} from 'date-fns';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import {COLORS} from '../constants/colors';
 import Header from '../components/Header';
 import AppStyle from '../assets/styles/AppStyle';
-import { Image } from 'react-native';
 import Ionicon from '@react-native-vector-icons/ionicons';
 import AnalogClock from '../components/AnalogClock';
 import TimeZones from '../components/TimeZones';
-
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import AllTimeZones from './AllTimeZones';
+import { observer } from 'mobx-react-lite';
+import { useNavigation } from '@react-navigation/native';
+import { ClockStore } from '../store';
+import { FLOATING_FOOTER_HEIGHT, SPACING } from '../constants';
 
 
 interface DateTimeProps extends PropsWithChildren {
@@ -41,10 +45,8 @@ const ClockStyle = StyleSheet.create({
 });
 
 const DateTime = ({date} : DateTimeProps) =>{
-  const dateStr = `${format(new Date(), 'EE, LLL dd yyy')}`;
-  const timeStr = `${format(new Date(), 'hh : mm aa')}`;
-  const hour = `${date.getHours()}`;
-  const min = `${date.getUTCMinutes()}`;// format hour and min
+  const dateStr = `${format(date, 'EE, LLL dd yyy')}`;
+  const timeStr = `${format(date, 'hh : mm aa')}`;
   return (
     <View>
       <View style={ClockStyle.digitalClockContainer}>
@@ -58,9 +60,6 @@ const DateTime = ({date} : DateTimeProps) =>{
           flexDirection: 'row',
           justifyContent: 'flex-start'
           }}>
-          {/* <Text style={ClockStyle.digitalClock}>{hour}</Text>
-          <Separator style={ClockStyle.digitalClock} />
-          <Text style={ClockStyle.digitalClock}>{min}</Text> */}
         </View>
       </View>
     </View>
@@ -68,73 +67,89 @@ const DateTime = ({date} : DateTimeProps) =>{
 }
 
 
-const Clock = (() => {
+const Clock = observer(({ navigation }:any) => {
   const [date, setDate] = useState(new Date());
   const windowHeight = Dimensions.get('window').height;
-  const floatingFooter = 100;
-  const spacing = 70;
-  const screenHeight = windowHeight - (floatingFooter + spacing);
+  const screenHeight = windowHeight - (FLOATING_FOOTER_HEIGHT + SPACING);
 
-  const CurrentDateTime = new Date();
   return (
-    <View
-      style={{
-        ...AppStyle.container,
-        marginBottom: 0,
-        position: 'relative',
-        height: screenHeight,
-      }}
-      >
-      <Header title='World clock' hasAdd />
-      <View style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-      }}>
-        <View style={{ flex: 1}}>
-          <Text style={{
-            fontSize: 18,
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View
+          style={{
+            ...AppStyle.container,
+            marginBottom: 0,
+            position: 'relative',
+            height: screenHeight,
+          }}
+        >
+          <Header
+            hasAdd
+            title='World clock'
+            onAdd={() => {
+              navigation.navigate('Timezones');
+            }}
+          />
+          <View style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}>
+            <View style={{ flex: 1 }}>
+              <Text style={{
+                fontSize: 18,
+                marginBottom: 10,
+                color: 'teal',
+                fontWeight: 500
+              }}>Local Time</Text>
+              <DateTime date={new Date()} />
+            </View>
+            <View style={{}}>
+              <AnalogClock />
+            </View>
+          </View>
+          <View style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
             marginBottom: 10,
-            color: 'teal',
-            fontWeight: 500
-            }}>Local Time</Text>
-          <DateTime date={new Date()} />
+          }}>
+            <Ionicon name='heart' color={'red'} size={18} />
+            <Text style={{
+              fontSize: 18,
+              fontWeight: 500,
+              marginLeft: 10,
+            }}>Saved places</Text>
+          </View>
+          <TimeZones
+            data={Object.values(ClockStore.favorites)}
+            localTimezone={ClockStore.localTimezone}
+          />
         </View>
-        <View style={{}}>
-          <AnalogClock />
-        </View>
-      </View>
-      <View style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        marginBottom: 10,
-        }}>
-        <Ionicon name='heart' color={'red'} size={18} />
-        <Text style={{
-          fontSize: 18,
-          fontWeight: 500,
-          marginLeft: 10,
-        }}>Saved places</Text>
-      </View>
-
-      <TimeZones />
-    </View>)
-})
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
+});
 
 const ClockStackNavigator = createNativeStackNavigator();
 
 export const ClockStackScreen = () => {
   return (
     <ClockStackNavigator.Navigator>
-      <ClockStackNavigator.Screen name="Clock" options={{ header: () => null }}>
-        {(props: any) => <Clock {...props} />}
-      </ClockStackNavigator.Screen>
+      <ClockStackNavigator.Group>
+        <ClockStackNavigator.Screen name="Clock" options={{ header: () => null }}>
+          {(props: any) => <Clock {...props} />}
+        </ClockStackNavigator.Screen>
+      </ClockStackNavigator.Group>
+      <ClockStackNavigator.Group screenOptions={{ presentation: 'modal' }}>
+        <ClockStackNavigator.Screen name="Timezones">
+          {(props:any) => <AllTimeZones {...props} />}
+        </ClockStackNavigator.Screen>
+      </ClockStackNavigator.Group>
     </ClockStackNavigator.Navigator>
   );
 }
 
 export default Clock;
-

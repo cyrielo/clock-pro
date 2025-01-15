cf7e35b30d730014b85a7bf2ab4af22c4f38ef04
Cyril Ologho <cyrielo911@gmail.com>
2025-01-15 16:29:31 -0500

    update clock store

--- src/store/ClockStore.ts
+++ src/store/ClockStore.ts
@@ -1,71 +1,84 @@
 import { runInAction, makeAutoObservable } from 'mobx';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-//import { formatInTimeZone } from 'date-fns-tz'
 import { Place } from '../types';
-import { getAllTimezones, TimezoneName } from 'countries-and-timezones';
+import { getAllCountries, getAllTimezones, TimezoneName } from 'countries-and-timezones';
 
 export default class ClockStore {
-  favorites:Place[] = [];
+  favorites = {} as Record<string, Place>;
+  allPlaces = {} as Record<string, Place>;
   localTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   private key = '__CLOCK__KEY__';
 
   constructor() {
-    this.allPlaces();
+    makeAutoObservable(this);
+    this.setFavoritesPlaces();
+    this.setAllPlaces();
   }
 
-  allPlaces() {
+  async setAllPlaces() {
     const allTimezones = getAllTimezones()
-    const k = Object.keys(allTimezones).slice(8, 20);
-
-    k.forEach((timeZone, idx) => {
-      const name = timeZone as TimezoneName;
-      const nameSplit = name.split('/');
-
-      const code = allTimezones[name].countries[0];
-      const location = `${nameSplit[nameSplit.length - 1].split('_').join(' ')}, ${code}`;
-      const utcOffset = allTimezones[name].utcOffset;
-      const dstOffset = allTimezones[name].dstOffset;
-
-      const place:Place = {
-        location,
-        timeZone,
-        code: code.toLowerCase(),
-        utcOffset,
-        dstOffset,
-        flag: `https://flagcdn.com/w320/${code.toLowerCase()}.png`,
-        isDayLight: (utcOffset === dstOffset),
-      };
-      runInAction(() => {
-        this.favorites.push(place);
-      });
-      console.log('place -> ', place);
+    const countries = getAllCountries();
+    const k = Object.keys(allTimezones);
+    const places = {} as Record<string, Place>;
+    for (let i = 0; i < k.length; i++) {
+      const timeZone = k[i] as TimezoneName;
+      const nameSplit = timeZone.split('/');
+      const code = allTimezones[timeZone].countries[0];
+      const location = `${nameSplit[nameSplit.length - 1].split('_').join(' ')}`;
+      const utcOffset = allTimezones[timeZone].utcOffset;
+      const dstOffset = allTimezones[timeZone].dstOffset;
+      const isFave =  Object.hasOwn(this.favorites, location);
+      if (!(countries[code])) {continue}
+      try {
+        const place: Place = {
+          location,
+          timeZone,
+          code,
+          utcOffset,
+          isFave,
+          dstOffset,
+          continent: nameSplit[0],
+          country: (countries[code]) ? countries[code].name : '',
+          flag: `https://flagcdn.com/w320/${code.toLowerCase()}.png`,
+          isDayLight: (utcOffset === dstOffset),
+        };
+        places[location] = place;
+      } catch(e) {
+        console.error('error happened ->', e);
+      }
+    }
+    runInAction(() => {
+      this.allPlaces = Object.assign(this.allPlaces, places);
     });
   }
-  addFavorite(place: Partial<Place>) {
-    /**
-     export type Place = {
-      location: string;
-      timeZone: string;
-      flag: string;
-      hoursDiff: number;
-      date: Date;
-      isDaytime: boolean;
-      isDayLight: boolean;
-    };
-    */
-    // const newPlace:Place = {
-    //   ...place,
-    //   flag: 'https://flagcdn.com/w320/us.png',
-    //   date: new Date(),
-    // };
-    // runInAction(() => {
-    //   this.favorites.push(newPlace);
-    // });
 
+  async addFavorite(place: Place) {
+    runInAction(() => {
+      if (!this.favorites[place.location]) {
+        this.favorites[place.location] = Object.assign(place, { isFave: true });
+      }
+    });
+    await this.setAllPlaces();
+    await AsyncStorage.setItem(this.key, JSON.stringify(this.favorites));
   }
 
-  fetchFavorites() {
-
+  async removeFavorite(place:Place) {
+    runInAction(() => {
+      delete this.favorites[place.location];
+    });
+    await this.setAllPlaces();
+    await AsyncStorage.setItem(this.key, JSON.stringify(this.favorites));
   }
 
+  private async getSavedPlaces() :Promise<Record<string, Place>> {
+    const favesString = await AsyncStorage.getItem(this.key) || null;
+    return (favesString != null) ? JSON.parse(favesString) : this.favorites;
+  }
+
+  async setFavoritesPlaces() {
+    const faves = await this.getSavedPlaces();
+    runInAction(() => {
+      this.favorites = faves;
+    });
+  }
 }

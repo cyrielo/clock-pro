38fbe90ce6980d09b6fd89f780f7523b32182c05
Cyril Ologho <cyrielo911@gmail.com>
2025-01-15 20:38:35 -0500

    update Timer ui

--- src/screens/Timer.tsx
+++ src/screens/Timer.tsx
@@ -1,81 +1,127 @@
-import React from 'react';
-import { View, Text, ScrollView, ViewStyle } from 'react-native';
+import React, { FunctionComponent, ReactElement, ReactNode } from 'react';
+import { View, Text,  Dimensions, TouchableOpacity } from 'react-native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Section from '../components/Section';
-import { CircularCard } from '../components/Card';
+import Ionicon from '@react-native-vector-icons/ionicons';
 import Header from '../components/Header';
-import { ClockValueStyle } from '../assets/styles/AppStyle';
-import Button from '../components/Button';
-import { COLORS } from '../constants/colors';
-import Ionicons from '@react-native-vector-icons/ionicons';
+import {observer} from 'mobx-react-lite';
+import { FLOATING_FOOTER_HEIGHT, SPACING } from '../constants';
+import CircularProgressBar from '../components/CircularProgressBar';
+import Pulsate from '../components/Pulsate';
+
+const GRID = {length: 3, height: 4};
 const TimerStackNavigator = createNativeStackNavigator();
 
-interface SeparatorProps {
-  style?: ViewStyle;
-}
-const Separator = ({ style }: SeparatorProps) => {
-  return (<Text style={{ ...style }}>:</Text>);
+
+type ColProps = {
+  row: number;
 }
 
-const LapControlView = () => {
+type RowProps = {
+  Col: FunctionComponent<ColProps>;
+}
+
+const TimerItem = () => {
   return (
-    <View style={{ marginTop: 20, height: 65, marginHorizontal: 40 }}>
-      <View style={{ ...ClockValueStyle.controlsBTNgrp, justifyContent: 'space-between' }}>
-        <Button style={{ ...ClockValueStyle.controlsBTN }}>
-          <Ionicons name='stop' size={28} />
-        </Button>
-        <Button style={{ ...ClockValueStyle.controlsBTN, backgroundColor: COLORS.Blue1 }}>
-          <Ionicons name='pause' color={COLORS.Light} size={28} />
-        </Button>
-      </View>
-    </View>
+    <>
+      <Pulsate duration={1000}>
+        <CircularProgressBar
+          size={100}
+          strokeWidth={3}
+          progress={10}>
+          <Text
+            style={{
+              fontSize: 16,
+              letterSpacing: 1.5,
+              fontWeight: 400,
+              textAlign: 'center'
+            }}>
+            24:03:15
+          </Text>
+        </CircularProgressBar>
+      </Pulsate>
+      <Ionicon
+        style={{
+          borderWidth: 1,
+          right: 2,
+          bottom: 2,
+          position: 'absolute',
+          borderColor: 'transparent'
+        }}
+        name='notifications'
+        size={16}
+        color={'#fff'}
+      />
+    </>
   )
 }
 
-const Timer = (() => {
-  return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={{
-        marginTop: 20,
-        marginBottom: 0,
-        marginHorizontal: 20,
-        paddingBottom: 130,
+const Rows: React.FC<RowProps> = ({ Col }) => (
+  Array.from({ length: GRID.height }).map((_, index) => {
+    return (
+      <View
+        key={index}
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginVertical: 15,
+          alignItems: 'center'
         }}>
-        <Header title='Timer' />
-        <CircularCard>
-          <View style={{ marginVertical: 10 }}>
-            <Text style={{
-              fontSize: 18,
-              fontWeight: '500', }}>4min</Text>
-          </View>
-          <View style={{
-            display: 'flex',
-            flexDirection: 'row',
-            marginVertical: 10,
-            alignItems: 'baseline'
-          }}>
-            <View style={{ marginRight: 15, }}>
-              <Text style={{ ...ClockValueStyle.values }}>02</Text>
-              <Text style={{ ...ClockValueStyle.labels }}>hour</Text>
-            </View>
-            <Separator style={{ ...ClockValueStyle.separator }} />
-            <View style={{ marginRight: 15 }}>
-              <Text style={{ ...ClockValueStyle.values }}>18</Text>
-              <Text style={{ ...ClockValueStyle.labels }}>min</Text>
-            </View>
-            <Separator style={{ ...ClockValueStyle.separator }} />
-            <View style={{ marginRight: 15 }}>
-              <Text style={{ ...ClockValueStyle.values }}>56</Text>
-              <Text style={{ ...ClockValueStyle.labels }}>sec</Text>
-            </View>
-          </View>
-          <View style={{ marginVertical: 15 }}>
-            <Text style={{ ...ClockValueStyle.labels }}>Paused</Text>
-          </View>
-        </CircularCard>
-        <LapControlView />
+        <Col row={index} />
       </View>
-    </ScrollView>)
+    )
+  })
+);
+const Cols: React.FC<ColProps> = ({row}) => {
+  return Array.from({ length: GRID.length }).map((_, index) => {
+    return (
+      <TouchableOpacity
+        key={index}
+        onPress={() => {
+          console.log('row ->', row);
+          console.log('Pressed cell', index);
+        }}
+        style={{
+          flex: 1,
+          borderWidth: 1,
+          borderColor: 'rgba(100,100,100,0.2)',
+          backgroundColor: 'rgba(100,100,100,0.5)',
+          marginHorizontal: 7,
+          height: 110,
+          borderRadius: 10,
+          padding: 4,
+          justifyContent: 'flex-end',
+        }}
+      >
+      </TouchableOpacity>
+    )
+  });
+};
+const Timer = observer(() => {
+    const windowHeight = Dimensions.get('window').height;
+    const screenHeight = windowHeight - (FLOATING_FOOTER_HEIGHT + SPACING);
+  return (
+    <View style={{
+      display: 'flex',
+      marginTop: 20,
+      marginBottom: 0,
+      marginHorizontal: 20,
+      paddingBottom: 130,
+      height: screenHeight
+    }}>
+      <Header title='Timer' hasAdd={true} />
+      <View style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center',
+        margin: 'auto',
+        marginTop: 35,
+      }}>
+        <Rows Col={Cols} />
+      </View>
+    </View>)
 });
 
 export const TimerStackScreen = () => {
@@ -88,5 +134,5 @@ export const TimerStackScreen = () => {
     </TimerStackNavigator.Navigator>
   );
 }
-
+Timer.displayName = 'Timer';
 export default Timer;

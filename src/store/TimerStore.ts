5893dc0118ec7dd300436a34cf4c4cb5e3b16491
Cyril Ologho <cyrielo911@gmail.com>
2025-01-18 17:00:39 -0500

    add timer store controls

--- src/store/TimerStore.ts
+++ src/store/TimerStore.ts
@@ -1,8 +1,11 @@
 import { runInAction, makeAutoObservable } from "mobx";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Timer } from "../types";
+
 export default class TimerStore {
   timer = {} as Record<string, Timer>
+  timerModalVisibility = false;
+  activeColumnKey = '';
   private key = '__TIMER_STORE_';
 
   constructor() {
@@ -10,20 +13,35 @@ export default class TimerStore {
     this.setTimer();
   }
 
-  async toggleTimer(index:string) {
+  async toggleTimer(activeColumnKey:string) {
     runInAction(() => {
-      const timer = this.timer[index];
+      const timer = this.timer[activeColumnKey];
       timer.isPaused = !timer.isPaused;
-      this.timer[index] = timer;
+      this.timer[activeColumnKey] = timer;
     });
     await this.persistTimer();
   }
 
-  async addTimer(index: string, timer: Timer) {
+  async addTimer(activeColumnKey: string, timer: Timer) {
     runInAction(() => {
-      this.timer[index] = timer;
+      this.timer[activeColumnKey] = timer;
     });
     await this.persistTimer();
+  }
+
+  toggleTimerModalVisibility(activeColumnKey?:string) {
+    runInAction(() => {
+      this.timerModalVisibility = !this.timerModalVisibility;
+      if (activeColumnKey) {
+        this.activeColumnKey = activeColumnKey;
+      }
+    });
+  }
+
+  setActiveColumn(activeColumnKey:string) {
+    runInAction(() => {
+      this.activeColumnKey = activeColumnKey;
+    });
   }
 
   async deleteTimer(index:string) {
@@ -40,6 +58,10 @@ export default class TimerStore {
   private async fetchTimer() {
     const timerStr = await AsyncStorage.getItem(this.key) || null;
     return (timerStr != null) ? JSON.parse(timerStr) : this.timer;
+  }
+
+  private async clearTimer () {
+    await AsyncStorage.removeItem(this.key);
   }
 
   async setTimer() {

9010a457bebe6ce08175249967fba6003eeb5f8d
Cyril Ologho <cyrielo911@gmail.com>
2025-01-19 01:55:43 -0500

    fix timer reset

--- src/screens/Timer.tsx
+++ src/screens/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
-import { View, Text,  Dimensions, FlatList, TouchableOpacity, Modal, Button, TextInput } from 'react-native';
+import { View, Text,  Dimensions, TouchableOpacity, Modal } from 'react-native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Ionicon from '@react-native-vector-icons/ionicons';
 import Header from '../components/Header';
@@ -16,10 +16,12 @@ const GRID = {length: 3, height: 4};
 const TimerStackNavigator = createNativeStackNavigator();
 
 type ColProps = {
+  timerRecord: Record<string, TimerType>;
   row: number;
 }
 
 type RowProps = {
+  timerRecord: Record<string, TimerType>;
   Col: FunctionComponent<ColProps>;
 }
 
@@ -28,12 +30,11 @@ interface TimerItemProps {
 }
 
 const TimerItem = observer(({ columnKey }: TimerItemProps) => {
-  const { color, duration, isPaused, label, sound, isComplete } = TimerStore.timer[columnKey];
+  const { duration, isPaused, label, sound, reset } = TimerStore.timer[columnKey];
   const countDownRef = useRef<NodeJS.Timeout|number>(0);
   const [countDown, setCountDown] = useState(duration);
-  //const [timerProgress, se]
   const { hours = 0, minutes = 0, seconds = 0 } = getTimeObj(countDown);
-  const isSilent = sound == 'silent';
+  const isSilent = (sound == 'silent');
   const fiftyPercent = duration * 0.6;
   const tenPercent = duration * 0.2;
   const timerProgress = (countDown / duration) * 100 > 0 ? (countDown / duration) * 100 : 0;
@@ -42,25 +43,30 @@ const TimerItem = observer(({ columnKey }: TimerItemProps) => {
   const countDownComplete = countDown <= -1000;
 
   useEffect(() => {
-    if (!isPaused && !isComplete) {
-      if (countDownComplete) { setCountDown(duration); } // reset duration
+    if (!isPaused ) {
+      if (countDownComplete || reset) {
+        setCountDown(duration);// reset duration
+        const update = { reset: false } as TimerType; // completed reset
+        TimerStore.updateTimer(columnKey, update);
+      } 
       countDownRef.current = setInterval(() => {
         setCountDown(countDown - 1000);
       }, 1000);
       if (countDown == -1000) {
         clearInterval(countDownRef.current);
-        const update = { isComplete: true, isPaused: true } as TimerType;
+        const update = { isPaused: true } as TimerType;
         TimerStore.updateTimer(columnKey, update);
       }
     }
     return () => {
       clearInterval(countDownRef.current);
     };
-  }, [isPaused, isComplete, pulseSpeed, countDown, duration]);
+  }, [isPaused, reset, pulseSpeed, countDown, duration]);
   return (
     <>
       <Pulsate isPaused={isPaused} duration={pulseSpeed}>
         <CircularProgressBar
+          isPaused={isPaused}
           size={100}
           strokeWidth={3}
           progress={timerProgress}>
@@ -100,7 +106,7 @@ const TimerItem = observer(({ columnKey }: TimerItemProps) => {
   )
 });
 
-const Rows: React.FC<RowProps> = ({ Col }) => (
+const Rows: React.FC<RowProps> = ({ Col, timerRecord }) => (
   Array.from({ length: GRID.height }).map((_, index) => {
     return (
       <View
@@ -112,16 +118,17 @@ const Rows: React.FC<RowProps> = ({ Col }) => (
           marginVertical: 15,
           alignItems: 'center'
         }}>
-        <Col row={index} />
+        <Col timerRecord={timerRecord} row={index} />
       </View>
     )
   })
 );
-const Cols: React.FC<ColProps> = ({row}) => {
+const Cols: React.FC<ColProps> = ({ row, timerRecord }) => {
   return Array.from({ length: GRID.length }).map((_, index) => {
     const columnKey = `${row}_${index}`;
-    const hasTimer = Object.hasOwn(TimerStore.timer, columnKey);
-    const timer = hasTimer ? TimerStore.timer[columnKey] as TimerType : {} as TimerType;
+    const hasTimer = Object.hasOwn(timerRecord, columnKey);
+    console.log('hasTimer', hasTimer);
+    const timer = hasTimer ? timerRecord[columnKey] as TimerType : {} as TimerType;
     return (
       <TouchableOpacity
         key={index}
@@ -129,10 +136,9 @@ const Cols: React.FC<ColProps> = ({row}) => {
           TimerStore.toggleTimerModalVisibility(columnKey);
         }}
         onPress={() => {
-          const timer = TimerStore.timer[columnKey];
           if (timer) {
             const update = { isPaused: !timer.isPaused } as TimerType;
-            update.isComplete = (timer.isComplete) ? false: timer.isComplete;
+            //update.reset = (timer.reset) ? false : timer.reset;
             TimerStore.updateTimer(columnKey, update);
           } else {
             TimerStore.toggleTimerModalVisibility(columnKey);
@@ -161,6 +167,7 @@ const Cols: React.FC<ColProps> = ({row}) => {
 const Timer = observer(() => {
   const windowHeight = Dimensions.get('window').height;
   const screenHeight = windowHeight - (FLOATING_FOOTER_HEIGHT + SPACING);
+  const timerRecord = Object.keys(TimerStore.timer).length ? TimerStore.timer : {};
   return (
     <SafeAreaProvider>
       <SafeAreaView>
@@ -188,7 +195,7 @@ const Timer = observer(() => {
           paddingBottom: 130,
           height: screenHeight
         }}>
-          <Header title='Timer' hasAdd={true} />
+          <Header title='Timer' hasAdd={false} />
           <View style={{
             display: 'flex',
             justifyContent: 'center',
@@ -198,7 +205,7 @@ const Timer = observer(() => {
             margin: 'auto',
             marginTop: 35,
           }}>
-            <Rows Col={Cols} />
+            <Rows timerRecord={timerRecord} Col={Cols} />
           </View>
         </View>
       </SafeAreaView>

479c355f3b0fa233a74e1e4750cfb294560fddb7
Cyril Ologho <cyrielo911@gmail.com>
2025-01-21 16:41:01 -0500

    complete AlarmStore featuresg

--- src/store/AlarmStore.ts
+++ src/store/AlarmStore.ts
@@ -1,10 +1,11 @@
 import { makeObservable, observable, action, runInAction } from 'mobx';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alarm } from '../types';
+import { formatTimeString } from '../utils/stringUtils';
 
 export default class AlarmStore {
 
-  alarms:Alarm[] = [];
+  alarms: Record<string, Alarm> = {};
 
   constructor() {
     makeObservable(this, {
@@ -16,56 +17,54 @@ export default class AlarmStore {
     this.loadAlarms();
   }
   private _ALARM_KEY = 'ALARM_KEY';
-
   async loadAlarms() {
     const alarms = await this.getAlarms();
-    this.setAlarms(alarms);
-  }
-  async createAlarm(alarm: Alarm) {
-    const allAlarms = await this.getAlarms();
-    allAlarms.push(alarm);
-    this.setAlarms(allAlarms);
-    this.save(allAlarms);
-  }
-
-  setAlarms(alarms: Alarm[]) {
     runInAction(() => {
       this.alarms = alarms;
-    })
+    });
   }
-
-  async updateAlarm(alarm:Alarm) {
-    const allAlarms = await this.getAlarms();
-    for (let idx = 0; idx < allAlarms.length; idx++) {
-      if (allAlarms[idx].id === alarm.id) {
-        allAlarms[idx] = { ...alarm };
-        break;
-      }
+  async createAlarm(alarm: Alarm, prevKey?:string) {
+    if (prevKey) {
+      this.updateAlarm(prevKey, alarm);
+      return;
     }
-    this.setAlarms(allAlarms);
-    this.save(allAlarms);
+    const key = this.generateAlarmKey(alarm);
+    runInAction(() => {
+      this.alarms[key] = alarm;
+    });
+    this.persistAlarm();
   }
 
-  async deleteAlarm(alarm: Alarm) {
-    const allAlarms = await this.getAlarms();
-    for (let idx = 0; idx < allAlarms.length; idx++) {
-      if (allAlarms[idx].id === alarm.id) {
-        allAlarms.splice(idx, 1);
-        break;
-      }
-    }
-    this.setAlarms(allAlarms);
-    this.save(allAlarms);
+  async updateAlarm(key:string, updatePartial:Partial<Alarm>) {
+    runInAction(() => {
+      const currentAlarm = this.alarms[key];
+      const update = Object.assign(currentAlarm, updatePartial);
+      this.alarms[key] = update;
+    });
+    this.persistAlarm();
   }
 
-  async save(alarms : Alarm[]) {
-    const alarmObjsStr = JSON.stringify(alarms);
+  async deleteAlarm(key:string) {
+    runInAction(() => {
+      delete this.alarms[key];
+    });
+    this.persistAlarm();
+  }
+
+  generateAlarmKey(a:Alarm):string {
+    const daysLen = a.weekdays.length;
+    const timestring = formatTimeString(new Date(a.timestamp));
+    return `${timestring}_${daysLen}`;
+  }
+
+  async persistAlarm() {
+    const alarmObjsStr = JSON.stringify(this.alarms);
     return await AsyncStorage.setItem(this._ALARM_KEY, alarmObjsStr);
   }
 
-  async getAlarms() : Promise<Alarm[]> {
+  async getAlarms() : Promise<Record<string, Alarm>> {
     const allAlarms = await AsyncStorage.getItem(this._ALARM_KEY);
-    return (allAlarms !== null) ? JSON.parse(allAlarms) as Alarm[] : [];
+    return (allAlarms !== null) ? JSON.parse(allAlarms) : {};
   }
 };
 

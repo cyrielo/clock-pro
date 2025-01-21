b80da4379906802f80ecc9c94b4242cd1730baba
Cyril Ologho <cyrielo911@gmail.com>
2025-01-20 22:00:47 -0500

    update manage alarm design

--- src/screens/ManageAlarm.tsx
+++ src/screens/ManageAlarm.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, Button,  } from 'react-native';
+import { View, Text, Button, TouchableOpacity,  } from 'react-native';
 import RNDateTimePicker, { DateTimePickerAndroid, DateTimePickerEvent } from '@react-native-community/datetimepicker';
 import { ScreenWithNavigation } from '../types';
 import ScrollPicker from 'react-native-wheel-scrollview-picker';
+import { CircularCard } from '../components/Card';
+import Ionicon from '@react-native-vector-icons/ionicons';
 
 const ManageAlarm = ({navigation, route} : ScreenWithNavigation) => {
   const [date, setDate] = useState(new Date());
@@ -31,27 +33,50 @@ const ManageAlarm = ({navigation, route} : ScreenWithNavigation) => {
   //DateTimePickerAndroid.open({value: new Date(), mode: 'time'});
   return (
     <View>
-      <ScrollPicker
-        style={{borderWidth: 2, height: 100, width: 100}}
-        dataSource={["1", "2", "3", "4", "5", "6"]}
-        selectedIndex={1}
-        renderItem={(data, index) => {
-          return (
-            <View>
-              <Text>{data}</Text>
+      <View style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginHorizontal: 'auto',
+        marginTop: 15
+      }}>
+        <TouchableOpacity>
+          <CircularCard style={{}}>
+            <View style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              width: '100%',
+            }}>
+              <View style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center'
+                }}>
+                <Ionicon name='alarm' color={'#09c'} size={20} />
+                <Text style={{ fontSize: 16, marginLeft: 5, textAlign: 'center' }}>
+                {'Alarm on'}
+                </Text>
+              </View>
+              <Text style={{ fontSize: 38, marginVertical: 15,  width: '100%', textAlign: 'center' }}>
+                {'6:00 AM'}
+              </Text>
+              <Text style={{
+                marginHorizontal: 'auto',
+                fontSize: 16,
+                width: '85%',
+                textAlign: 'center'
+                }}>
+                {'- 12hrs 15mins'}
+              </Text>
+              <Text style={{ fontSize: 16, width: '100%', textAlign: 'center' }}>
+                {'remaining'}
+              </Text>
             </View>
-          );
-        }}
-        onValueChange={(data, selectedIndex) => {
-          //
-        }}
-        wrapperHeight={180}
-        wrapperBackground="#FFFFFF"
-        itemHeight={60}
-        highlightColor="#d8d8d8"
-        highlightBorderWidth={2}
-      />
-      <Button title='Show time picker' onPress={() => showTimepicker()} />
+          </CircularCard>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };

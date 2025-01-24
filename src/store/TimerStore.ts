6423736af3709051b5e182e90c5c0ab9cdbb109d
Cyril Ologho <cyrielo911@gmail.com>
2025-01-24 18:52:02 -0500

    refactor notification from TimerStore

--- src/store/TimerStore.ts
+++ src/store/TimerStore.ts
@@ -1,6 +1,7 @@
 import { runInAction, makeAutoObservable } from "mobx";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Timer } from "../types";
+import { cancelNotificationSchedule,  getTriggerNotificationIds, scheduleTimer } from "../services/NotificationServices";
 
 export default class TimerStore {
   timer = {} as Record<string, Timer>
@@ -13,22 +14,39 @@ export default class TimerStore {
     this.setTimer();
   }
 
-  async updateTimer(activeColumnKey:string, timer:Timer) {
+  async updateTimer(activeColumnKey:string, timer:Timer, initiator?:string) {
     runInAction(() => {
       this.timer[activeColumnKey] = Object.assign(this.timer[activeColumnKey], { ...timer });
     });
-    await this.persistTimer();
-  }
-
-  isTimerPaused(activeColumnKey: string) :boolean|undefined {
-    const timer = this.timer[activeColumnKey];
-    return (timer) ? timer.isPaused : undefined;
+    // handle notification triggers
+    if (timer.isPaused && !timer.isComplete) {
+      await cancelNotificationSchedule(timer.id);
+    }
+    if (!timer.isPaused) {
+      const notificationsIds = await getTriggerNotificationIds();
+      const diff = timer.duration - timer.elapsedTime;
+      const timestamp = Date.now() + diff;
+      if (notificationsIds.includes(timer.id)) {
+        if (timer.elapsedTime > 0) {
+          await cancelNotificationSchedule(timer.id);
+          await scheduleTimer(timer, timestamp);
+        }
+        await this.persistTimer();
+        return;
+      }
+      if (!timer.isComplete) {
+        await this.persistTimer();
+        await scheduleTimer(timer, timestamp);
+      }
+    }
   }
 
   async addTimer(activeColumnKey: string, timer: Timer) {
+    const timestamp = Date.now() + timer.duration;
     runInAction(() => {
       this.timer[activeColumnKey] = timer;
     });
+    await scheduleTimer(timer, timestamp);
     await this.persistTimer();
   }
 
@@ -48,10 +66,16 @@ export default class TimerStore {
   }
 
   async deleteTimer(activeColumnKey:string) {
-    runInAction(() => {
-      delete this.timer[activeColumnKey];
-    });
-    await this.persistTimer();
+    try {
+      await cancelNotificationSchedule(this.timer[activeColumnKey].id);
+      runInAction(() => {
+        delete this.timer[activeColumnKey];
+      });
+      await this.persistTimer();
+    } catch (error) {
+      console.error('failed to delete ->', error);
+    }
+
   }
 
   async persistTimer() {

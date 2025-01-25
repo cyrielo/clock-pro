b829224dafc77db60582401033a296a268c23164
Cyril Ologho <cyrielo911@gmail.com>
2025-01-25 01:20:22 -0500

    complete notification service for Alarms and timers

--- src/services/NotificationServices.ts
+++ src/services/NotificationServices.ts
@@ -1,6 +1,9 @@
 import notifee, { TimestampTrigger, TriggerType, AlarmType, AndroidImportance, AndroidVisibility, EventType } from '@notifee/react-native';
-import { Alarm, Timer, Notification } from '../types';
+import { Alarm, Timer, Notification, Weekdays, TriggerPayload } from '../types';
 import { COLORS } from '../constants/colors';
+import { fromZonedTime } from 'date-fns-tz';
+import { ClockStore } from '../store';
+import { add, format, getTime } from 'date-fns';
 
 export const scheduleTimer = async (timer:Timer, timestamp:number) => {
   const notificationTrigger: TimestampTrigger = {
@@ -11,7 +14,6 @@ export const scheduleTimer = async (timer:Timer, timestamp:number) => {
       type: AlarmType.SET_EXACT
     },
   };
-
   const notifciation: Notification = {
     id: timer.id,
     color: timer.color,
@@ -19,14 +21,18 @@ export const scheduleTimer = async (timer:Timer, timestamp:number) => {
     title: `⏰ ${timer.label}`,
     description: `Timer is complete`
   };
-  await triggerNotification(notificationTrigger, notifciation);
+  const payload:TriggerPayload = {
+    notifciation,
+    trigger: notificationTrigger,
+  }
+  await triggerNotification(payload);
 };
 
-export const cancelNotificationSchedule = async (id:string) => {
-  return await notifee.deleteChannel(id);
+export const cancelTimerNotification = async (id:string) => {
+  notifee.cancelTriggerNotification(id);
 }
 
-export const scheduleAlarm = async (alarm: Alarm) => {
+const a = async (alarm:Alarm, channelId?:string) => {
   const notificationTrigger: TimestampTrigger = {
     timestamp: alarm.timestamp,
     type: TriggerType.TIMESTAMP,
@@ -42,50 +48,140 @@ export const scheduleAlarm = async (alarm: Alarm) => {
     title: `⏰ ${alarm.label}`,
     description: 'Alarm'
   };
-  await triggerNotification(notificationTrigger, notifciation);
+  const payload:TriggerPayload = {
+    channelId: channelId ? channelId : notifciation.id,
+    notifciation,
+    trigger: notificationTrigger,
+    data: { alarm: JSON.stringify(alarm) }
+  }
+  await triggerNotification(payload);
+}
+
+const RescheduleAlarmInPlace = async (alarm:Alarm) => {
+  const timezone = ClockStore.localTimezone;
+  const date = fromZonedTime(new Date(alarm.timestamp), timezone);
+  const newDate = add(date, { days: 7, });
+  const newAlarm = Object.assign({}, alarm, {
+    timestamp: newDate.getTime()
+  });
+  await a(newAlarm);
+}
+
+export const ScheduleAlarm = async (alarm: Alarm) => {
+  try {
+    const timezone = ClockStore.localTimezone;
+    const alarmNotificationId = `${alarm.id}_alarm`; //adds alarm keyWord for alarmNotifications identity;
+    const today = fromZonedTime(new Date(), timezone);
+    // start alarm from nextDay if the datetime is in the past
+    if (alarm.timestamp < getTime(today)) {
+      const elapsedDate = fromZonedTime(new Date(alarm.timestamp), timezone);
+      const newDate = add(today, { hours: 24 });
+      newDate.setHours(elapsedDate.getHours());
+      newDate.setMinutes(elapsedDate.getMinutes());
+      newDate.setSeconds(elapsedDate.getSeconds());
+      alarm.timestamp = getTime(newDate);
+    }
+    if (!alarm.weekdays.length) {
+      await a(Object.assign({}, alarm, { id: alarmNotificationId }), alarmNotificationId);
+      return;
+    }
+    const weekdays =  alarm.weekdays; 
+    if (weekdays.length) {
+      const date = fromZonedTime(new Date(alarm.timestamp), timezone);
+      const daysInWeek = 7;
+      let match = 0;
+      for (let i = 0; i < daysInWeek; i++) {
+        const newDate = add(date, { days: i });
+        const weekday = format(newDate, 'eeee').toLowerCase() as Weekdays;
+        if (weekdays.includes(weekday)) {
+          match += 1;
+          const id = `${alarm.timestamp}_${weekday}_alarm`;
+          const timestamp = newDate.getTime();
+          const newAlarm = Object.assign({}, alarm, {timestamp, id});
+          await notifee.cancelTriggerNotification(id);
+          await a(newAlarm, alarmNotificationId);
+          // schedule Alarm for the week
+          if (match === alarm.weekdays.length) {
+            break;
+          }
+        }
+      }
+    }
+  } catch(error ) {
+    console.error('ScheduleAlarm::Error', error);
+  }
+
+};
+
+export const CancelAlarmSchedule = async (alarm:Alarm, channelId:string) => {
+  try {
+    const notifciationIds = [`${alarm.id}_alarm`];
+    for (let i = 0; i < alarm.weekdays.length; i++) {
+      const weekday = alarm.weekdays[i];
+      notifciationIds.push(`${alarm.timestamp}_${weekday}_alarm`);
+    }
+    await notifee.cancelAllNotifications(notifciationIds);
+    await notifee.deleteChannel(channelId);
+  } catch(error) {
+    console.error('Error while CancelAlarmSchedule ', error);
+  }
 };
 
 export const getTriggerNotificationIds = async() => {
   return await notifee.getTriggerNotificationIds();
 }
 
-const triggerNotification = async (trigger:TimestampTrigger, notifciation:Notification) => {
+const triggerNotification = async (payload: TriggerPayload) => {
   await notifee.requestPermission();
   try {
+    const channelId = payload.channelId ? payload.channelId : payload.notifciation.id;
     await notifee.createChannel({
-      id: notifciation.id,
-      name: notifciation.id,
-      sound: notifciation.sound,
+      id: channelId,
+      name: payload.notifciation.id,
+      sound: payload.notifciation.sound,
       bypassDnd: true,
       importance: AndroidImportance.HIGH
     }); 
     await notifee.createTriggerNotification({
-      title: notifciation.title,
-      body: notifciation.description,
+      title: payload.notifciation.title,
+      body: payload.notifciation.description,
+      data: payload.data,
       android: {
-        channelId: notifciation.id,
+        channelId,
         ongoing: true,
-        sound: notifciation.sound || 'default',
-        color: notifciation.color || 'blue',
+        sound: payload.notifciation.sound || 'default',
+        color: payload.notifciation.color || 'blue',
         importance: AndroidImportance.HIGH,
         lightUpScreen: true,
         loopSound: true,
         onlyAlertOnce: false,
         visibility: AndroidVisibility.PUBLIC,
-        pressAction: { id: notifciation.id,  },
+        pressAction: { id: payload.notifciation.id,  },
       },
-    }, trigger);
+    }, payload.trigger);
   } catch (e) {
     console.error('notifciation error', e);
   }
 }
+
 notifee.onForegroundEvent(async ({ type, detail }) => {
   switch (type) {
     case EventType.UNKNOWN:
     case EventType.DISMISSED:
     case EventType.PRESS:
       if (detail.notification &&  detail.notification.id) {
-        await cancelNotificationSchedule(detail.notification.id);
+        await notifee.cancelNotification(detail.notification.id);
+        if (detail.notification.id.includes('_alarm')) {
+          try {
+            const data = (detail.notification.data?.alarm as any) as string;
+            const alarm = (JSON.parse(data)) || {} as Alarm;
+            if (alarm.shouldRepeat) {
+              await RescheduleAlarmInPlace(alarm);
+            }
+          } catch(error) {
+            console.error('Failed to RescheduleAlarmInPlace', error);
+          }
+        }
       }
       break;
   }

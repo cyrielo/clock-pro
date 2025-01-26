1e27797dc2f5825daf3013be57ca7f307504ecc9
Cyril Ologho <cyrielo911@gmail.com>
2025-01-26 03:04:03 -0500

    update translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,10 @@
 import i18n, { LanguageDetectorModule, init, Resource } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './locales/en.json';
-import cnTranslations from './locales/en.json';
-import hiTranslations from './locales/en.json';
-import tlTranslations from './locales/en.json';
-import frTranslations from './locales/en.json';
+import cnTranslations from './locales/cn.json';
+import hiTranslations from './locales/hi.json';
+import tlTranslations from './locales/tl.json';
+import frTranslations from './locales/fr.json';
 import { PreferencesStore } from '../store';
 
 const languages: Resource = {
@@ -20,7 +20,6 @@ const languageDetector: LanguageDetectorModule = {
   type: 'languageDetector',
   detect: () => {
     const lang = PreferencesStore.preferences.language;
-    console.log('detected', lang);
     return lang;
   },
 };

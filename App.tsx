4290257c212910a5ec0da442d32f01a92e99319d
Cyril Ologho <cyrielo911@gmail.com>
2025-01-26 00:24:12 -0500

    remove Hydrate from App

--- App.tsx
+++ App.tsx
@@ -8,37 +8,33 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { useColorScheme, View, ActivityIndicator } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { DarkTheme, LightTheme } from './src/constants/colors';
 import _, { PreferencesStore } from './src/store';
 import TabNavigation from './src/navigations/TabNavigation';
 import { ThemeType } from './src/types';
 import { observer } from 'mobx-react-lite';
-
+import { I18nextProvider } from 'react-i18next';
+import i18n from './src/i18n';
 const Stack = createNativeStackNavigator();
 
 const App = observer(() => {
-    const userTheme = PreferencesStore.preferences.theme;
-    const systemTheme = useColorScheme() || 'light' as ThemeType;
-    const colorScheme = (userTheme === 'system') ? systemTheme : userTheme;
-    const theme = colorScheme == 'dark' ? DarkTheme : LightTheme;
-  if (!PreferencesStore.preferences.isHydrated) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#000" />
-      </View>
-    );
-  }
+  const userTheme = PreferencesStore.preferences.theme;
+  const systemTheme = useColorScheme() || 'light' as ThemeType;
+  const colorScheme = (userTheme === 'system') ? systemTheme : userTheme;
+  const theme = colorScheme == 'dark' ? DarkTheme : LightTheme;
   return (
-    <NavigationContainer theme={theme} >
-        <Stack.Navigator>
-          <Stack.Screen
-            name="TabNavigation"
-            component={TabNavigation}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
+    <I18nextProvider i18n={i18n}>
+      <NavigationContainer theme={theme} >
+          <Stack.Navigator>
+            <Stack.Screen
+              name="TabNavigation"
+              component={TabNavigation}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
       </NavigationContainer>
+    </I18nextProvider>
     );
 })
 

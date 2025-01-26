31763c795c2a5ff56153604cb4f579305949d6e1
Cyril Ologho <cyrielo911@gmail.com>
2025-01-25 19:22:58 -0500

    use theme color for AllTimeZone screen

--- src/screens/AllTimeZones.tsx
+++ src/screens/AllTimeZones.tsx
@@ -1,4 +1,3 @@
-import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { View, Text, ScrollView, Button, TextInput, Dimensions, KeyboardAvoidingView } from 'react-native';
 import TimeZones from '../components/TimeZones';
@@ -6,12 +5,14 @@ import { ClockStore } from '../store';
 import Ionicon from '@react-native-vector-icons/ionicons';
 import { observer } from 'mobx-react-lite';
 import { FLOATING_FOOTER_HEIGHT, SPACING } from '../constants';
+import { useTheme } from '@react-navigation/native';
+import { COLORS } from '../constants/colors';
 
 const AllTimeZones = observer(() => {
-  const navigation = useNavigation();
+  const theme = useTheme();
   const [searchString, setSearchString] = useState('');
-    const windowHeight = Dimensions.get('window').height;
-    const marginBottom = 60;
+  const windowHeight = Dimensions.get('window').height;
+  const marginBottom = 60;
   const screenHeight = windowHeight - (FLOATING_FOOTER_HEIGHT + SPACING + marginBottom);
   return (
     <KeyboardAvoidingView
@@ -23,17 +24,18 @@ const AllTimeZones = observer(() => {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-        borderWidth: 1.5,
-        borderColor: 'rgba(100,100,100,0.5)',
+        borderWidth: 1,
+        borderColor: theme.colors.border,
         borderRadius: 8,
         padding: 4,
         marginBottom: 15,
       }}>
-        <Ionicon name='search' size={24} />
+        <Ionicon name='search' color={COLORS.Grey} size={24} />
         <TextInput
-          style={{ minWidth: '80%', fontSize: 16, marginLeft: 5 }}
+          style={{ minWidth: '80%', fontSize: 16, marginLeft: 5, color: theme.colors.text }}
           placeholder='Search'
           value={searchString}
+          placeholderTextColor={COLORS.Grey}
           onChangeText={(text) => setSearchString(text)}
           />
       </View>

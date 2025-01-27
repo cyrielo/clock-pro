9290e5c81f1c579ccc412f5c32f5eb3cc9d379f1
Cyril Ologho <cyrielo911@gmail.com>
2025-01-27 14:59:48 -0500

    fix ui issues for stopwatch

--- src/screens/StopWatch.tsx
+++ src/screens/StopWatch.tsx
@@ -7,20 +7,30 @@ import LapControlView from '../components/LapControlView';
 import LapView from '../components/LapView';
 import Header from '../components/Header';
 import i18n from '../i18n';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import AppStyle from '../assets/styles/AppStyle';
 
 const StopWatch = observer(() => {
   const stopWatchRef = useRef<ReactNode|null>(null);
   const windowHeight = Dimensions.get('window').height;
   const floatingFooter = 100;
-  const spacing = 70;
+  const spacing = 100;
   const screenHeight = windowHeight - (floatingFooter + spacing);
   return (
-    <View style={{ height: screenHeight }} >
-      <Header title={i18n.t('stopwatch')} hasAdd={false} />
-      <StopwatchFace ref={stopWatchRef} />
-      <LapView />
-      <LapControlView stopWatchFaceHandle={stopWatchRef} />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View style={{
+          ...AppStyle.container,
+          height: screenHeight
+          }} >
+          <Header title={i18n.t('stopwatch')} hasAdd={false} />
+          <StopwatchFace ref={stopWatchRef} />
+          <LapView />
+          <LapControlView stopWatchFaceHandle={stopWatchRef} />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+
 )
 });
 
